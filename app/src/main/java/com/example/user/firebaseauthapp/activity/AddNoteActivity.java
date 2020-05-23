fc0d37867678d457ac5cbe2dbc7d782417182eb4
Awon <awonzaman@gmail.com>
2020-05-23 17:47:53 +0600

    code refactoring

--- app/src/main/java/com/example/user/firebaseauthapp/activity/AddNoteActivity.java
+++ app/src/main/java/com/example/user/firebaseauthapp/activity/AddNoteActivity.java
@@ -102,20 +102,6 @@ public class AddNoteActivity extends AppCompatActivity implements View.OnClickLi
                 } else
                     saveNotes(notesModel);
                 break;
-            /*case R.id.btn_speak_title:
-                Intent checkIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-                checkIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-                checkIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-                checkIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hello, How can I help you?");
-                startActivityForResult(checkIntent, MY_TITLE_CHECK_CODE);
-                break;
-            case R.id.btn_speak_details:
-                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hello, How can I help you?");
-                startActivityForResult(intent, MY_DETAILS_CHECK_CODE);
-                break;*/
         }
     }
 
